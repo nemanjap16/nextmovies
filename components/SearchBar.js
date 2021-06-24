233ec981778797ba7643335a1edb58c0c45122c4
unknown <necaintruder@gmail.com>
2021-06-24 12:11:35 +0200

    fix search

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,17 +1,21 @@
-// import env from "../utility/env";
-// import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function SearchBar() {
   const router = useRouter();
 
+  const submitSearch = (e) => {
+    if (e.code === "Enter") {
+      router.push(`movies/?movie_name=${e.target.value}`);
+    }
+  };
+
   return (
     <div className="flex mt-10 justify-center">
       <input
-        onChange={(e) => router.push(`movies/?movie_name=${e.target.value}`)}
+        onKeyPress={(e) => submitSearch(e)}
         type="text"
-        placeholder="search movies..."
-        className="bg-input rounded-xl w-50 sm:w-80 sm:focus:w-96  px-5 py-1  duration-300 ease-in-out outline-none"
+        placeholder="Type & press enter"
+        className="bg-input rounded-xl w-50 sm:w-80 sm:focus:w-96 px-5 py-1 duration-300 ease-in-out outline-none"
       />
     </div>
   );
